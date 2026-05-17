cea192fd6cea118c051bdb0a79d3c27b379c301c
mpriz <ddmpriza@csd.auth.gr>
2026-05-17 17:01:40 +0300

    Added scoring system and UI

--- Assets/Scripts/BulletCollision.cs
+++ Assets/Scripts/BulletCollision.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 // Script για collision detection
 public class BulletCollision : MonoBehaviour
 {
-    // Η function καλείται αυτόματα
-    // όταν γίνει collision
+    // Η function καλείται αυτόματα όταν γίνει collision
     void OnCollisionEnter(Collision collision)
     {
         // Ελέγχουμε αν το αντικείμενο
         // που χτυπήσαμε έχει tag "Target"
         if (collision.gameObject.CompareTag("Target"))
         {
+            // Αυξάνουμε το score
+            ScoreManager.instance.AddScore();
+
             // Καταστρέφουμε το target
             Destroy(collision.gameObject);
 
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+// Script διαχείρισης score
+public class ScoreManager : MonoBehaviour
+{
+    // Singleton-style access
+    public static ScoreManager instance;
+
+    // Current score
+    int score = 0;
+
+    // Reference στο UI text
+    public TMP_Text scoreText;
+
+    void Awake()
+    {
+        // Αποθηκεύουμε reference
+        instance = this;
+    }
+
+    // Function που αυξάνει score
+    public void AddScore()
+    {
+        // Αύξηση score
+        score++;
+
+        // Update UI
+        scoreText.text = "Score: " + score;
+    }
+}
